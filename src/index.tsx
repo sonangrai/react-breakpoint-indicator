d283759c2aaea6889008b38d2a6cf326e42b72fb
sonangrai <shonahangrae@gmail.com>
2024-12-19 14:44:03 +0545

    feat: drag and auto position

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import useDragging from "./hooks/useDragging";
 
 type BreakpointIndicatorProps = {
@@ -11,7 +11,8 @@ type BreakpointIndicatorProps = {
  */
 function BreakpointIndicator({ moreStyle }: BreakpointIndicatorProps) {
   const [width, setWidth] = useState<number>(0);
-  const [draggingRef, x, y, isDragging] = useDragging();
+  const draggingRef = useRef<HTMLDivElement>(null);
+  const { pos, isDragging } = useDragging(draggingRef);
 
   useEffect(() => {
     function handleResize() {
@@ -27,34 +28,76 @@ function BreakpointIndicator({ moreStyle }: BreakpointIndicatorProps) {
     };
   }, []);
 
-  if (process.env.NODE_ENV === "production") return null;
+  // This is to make the indicator stick to the right or bottom of the screen when not dragging
+  useEffect(() => {
+    let top;
+    let left;
 
-  function renderString(width: number) {
+    function updatePosition() {
+      const element = draggingRef.current;
+
+      if (element && !isDragging) {
+        if (pos.x < window.innerWidth / 2) {
+          left = `0%`;
+        } else {
+          left = `calc(100% - ${element?.clientWidth}px)`;
+        }
+
+        if (pos.y < window.innerHeight / 2) {
+          top = `0%`;
+        } else {
+          top = `calc(100% - ${element?.clientHeight}px)`;
+        }
+
+        element.style.setProperty("left", `${left}`);
+        element.style.setProperty("top", `${top}`);
+      }
+    }
+
+    window.addEventListener("resize", updatePosition);
+
+    updatePosition();
+
+    return () => {
+      window.removeEventListener("resize", updatePosition);
+    };
+  }, [pos, isDragging]);
+
+  const percentagePosition = useCallback((pos: { x: number; y: number }) => {
+    return {
+      left: `${(pos.x / window.innerWidth) * 100}%`,
+      top: `${(pos.y / window.innerHeight) * 100}%`,
+    };
+  }, []);
+
+  const renderString = useCallback((width: number) => {
     let result = "";
     switch (true) {
       case width <= 640:
-        result = "sm";
+        result = "SM";
         break;
 
       case width <= 768:
-        result = "md";
+        result = "MD";
         break;
 
       case width <= 1024:
-        result = "lg";
+        result = "LG";
         break;
 
       case width <= 1280:
-        result = "xl";
+        result = "XL";
         break;
 
       default:
-        result = "2xl";
+        result = "2XL";
         break;
     }
 
     return result;
-  }
+  }, []);
+
+  if (process.env.NODE_ENV === "production") return null;
 
   return (
     <div
@@ -62,10 +105,11 @@ function BreakpointIndicator({ moreStyle }: BreakpointIndicatorProps) {
       style={{
         ...mainStyle,
         ...moreStyle,
-        left: `${x}px`,
-        top: `${y}px`,
+        left: percentagePosition(pos).left,
+        top: percentagePosition(pos).top,
         opacity: isDragging ? "0.5" : "1",
       }}
+      title={`${width}px`}
       draggable
     >
       {renderString(width)}
@@ -77,11 +121,12 @@ export default BreakpointIndicator;
 
 const mainStyle: React.CSSProperties = {
   position: "fixed",
-  backgroundColor: "#d81d1d",
+  backgroundColor: "rgb(179 21 116)",
   color: "#fff",
   padding: "10px",
   borderRadius: "5px",
   fontSize: "12px",
   zIndex: 999,
   cursor: "move",
+  transition: "all 0.2s ease",
 };
